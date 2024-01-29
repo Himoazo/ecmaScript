7d2a34acaf9adff3d320793c9a9179ad754d0c30
Hoomam <hoaz1600@student.miun.se>
2024-01-30 00:50:48 +0100

    fixat sortering enligt kurskod

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,32 @@
 "use strict";
+const nameEl = document.getElementById("code");
+const courseEl = document.getElementById("coursesList");
 
 window.onload = init;
+
 // Hämta information från API
 async function init(){
     try {
         const response = await fetch ("https://dahlgren.miun.se/ramschema_ht23.php");
         const courses = await response.json();
-
         showCourses(courses);
+        nameEl.addEventListener("click", function() {
+            courses.sort((a, b)=> (a.code > b.code)? 1 : -1);
+            courseEl.innerHTML = ""
+            showCourses(courses);
+           });
+        
     } catch (error){
         console.log(error);
         document.getElementById("error").innerHTML = "<p>Något gick fel</p>"
     }
 }
 
+// Skriva ut kursinfo till DOM
 function showCourses(courses){
-    const courseEl = document.getElementById("coursesList");
+    
+    
+    //Loopa genom objectet
     for (let course of courses){
         courseEl.innerHTML += `
         <tr>
@@ -26,3 +37,7 @@ function showCourses(courses){
         `
     }
 }
+
+
+
+
