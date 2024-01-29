c1b33562f8c671ba49857a7f3184b14c3e5db91b
Hoomam <hoaz1600@student.miun.se>
2024-01-29 15:06:25 +0100

    Created func init

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+window.onload = init;
+// Hämta information från API
+async function init(){
+    try {
+        const response = await fetch ("https://dahlgren.miun.se/ramschema_ht23.php");
+        const courses = await response.json();
+        console.log(courses);
+    } catch (error){
+        console.log(error);
+        document.getElementById("error").innerHTML = "<p>Något gick fel</p>"
+    }
+}
